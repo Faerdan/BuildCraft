1c5532370d549038102a3d4ae01bda4407ceace0
Mark Aherne <mark@warducks.com>
2020-06-12 17:06:08 +0100

    Added ElectriCraft to the moveLibsToMods batch file.

--- moveLibsToMods.bat
+++ moveLibsToMods.bat
@@ -1,6 +1,7 @@
 robocopy .\build\libs C:\Users\faerd\AppData\Roaming\.minecraft\mods\1.7.10 buildcraft-7.1.22.jar /MOV
 robocopy .\build\libs ..\DragonAPI\libs buildcraft-7.1.22-dev.jar
 robocopy .\build\libs ..\RotaryCraft\libs buildcraft-7.1.22-dev.jar
+robocopy .\build\libs ..\ElectriCraft\libs buildcraft-7.1.22-dev.jar
 robocopy .\build\libs ..\Binnie\libs buildcraft-7.1.22-dev.jar
 robocopy .\build\libs ..\Forestry\libs buildcraft-7.1.22-dev.jar
 robocopy .\build\libs ..\Railcraft\libs buildcraft-7.1.22-dev.jar
