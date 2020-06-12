dc494b86aac57a7e3eb6b72aceef018405ac31a5
Mark Aherne <mark@warducks.com>
2020-06-12 02:50:55 +0100

    Reduced tank capacity.

--- common/buildcraft/factory/TileTank.java
+++ common/buildcraft/factory/TileTank.java
@@ -31,7 +31,7 @@ import buildcraft.core.lib.fluids.TankManager;
 import buildcraft.core.lib.utils.BlockUtils;
 
 public class TileTank extends TileBuildCraft implements IFluidHandler {
-	public final Tank tank = new Tank("tank", FluidContainerRegistry.BUCKET_VOLUME * 16, this);
+	public final Tank tank = new Tank("tank", FluidContainerRegistry.BUCKET_VOLUME * 12, this);
 	public final TankManager<Tank> tankManager = new TankManager<Tank>(tank);
 	public boolean hasUpdate = false;
 	public boolean hasNetworkUpdate = false;
